c032d9c1a73798b28a700e9b56dbeed05debfeb5
Rico Picone <rpicone@stmartin.edu>
2022-11-19 22:41:17 -0800

    tightened up per joe edits

--- c/buffers/buffers.c
+++ c/buffers/buffers.c
@@ -1,14 +1,14 @@
 #include <time.h>
 #include <stdio.h>
 
-    int 
-main (void) {
+int main (void) {
     int i;
     FILE *fp;                               // file pointer
     clock_t tic = clock();                  // start clock
     fp = fopen("file.txt", "w+");           // open file to write
     for(i=0; i < 1000000; i++) {            // 1M writes
-        fputc('a', fp);                     // write 'a' repeatedly
+        // fputc('a', fp);                     // write 'a' repeatedly
+        // fflush(fp);
     }
     fclose(fp);                             // close file
     clock_t toc = clock();                  // stop clock
